8f2481d3fa90bfc010b634e97332b7fd56d8a268
shyndard <shyndard@gmail.com>
2019-01-23 16:00:03 +0100

    Ajout instruction jenkins + changement fonctionnement controller

--- src/main/java/fr/epsi/mspr/restapi/controller/ItemController.java
+++ src/main/java/fr/epsi/mspr/restapi/controller/ItemController.java
@@ -3,33 +3,40 @@ package fr.epsi.mspr.restapi.controller;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.epsi.mspr.restapi.service.AuthService;
 import fr.epsi.mspr.restapi.service.ItemService;
-import fr.epsi.mspr.restapi.service.JsonService;
 import fr.epsi.mspr.restapi.service.metier.dto.out.DtoOutEquipment;
 
 @RestController
 public class ItemController {
 
 	@Autowired
-    private ItemService itemService;
-	
+	private ItemService itemService;
+
 	@Autowired
-    private JsonService jsonService;
+	private AuthService authService;
 
 	@RequestMapping(value = "/item/getAll", produces = { MediaType.APPLICATION_JSON })
-    public @ResponseBody ResponseEntity<?> getAll(@RequestBody String result) {
-		
-		return ResponseEntity.ok(new DtoOutEquipment().setEquipements(itemService.getAll()));
-    }
-	
+	public @ResponseBody ResponseEntity<?> getAll(@RequestHeader(value="Authorization") String authorization, @RequestBody String body) {
+		if (authService.isValid(authorization)) {
+			return ResponseEntity.ok(new DtoOutEquipment().setEquipements(itemService.getAll()));
+		}
+		return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+	}
+
 	@RequestMapping(value = "/item/borrow")
-    public @ResponseBody ResponseEntity<?> borrowItem(@RequestBody String result) {
-		return ResponseEntity.ok(null);
-    }
+	public @ResponseBody ResponseEntity<?> borrowItem(@RequestHeader(value="Authorization") String authorization, @RequestBody String body) {
+		if (authService.isValid(authorization)) {
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+	}
 }
--- src/main/java/fr/epsi/mspr/restapi/service/JsonService.java
+++ src/main/java/fr/epsi/mspr/restapi/service/JsonService.java
@@ -1,5 +1,9 @@
 package fr.epsi.mspr.restapi.service;
 
+import fr.epsi.mspr.restapi.service.metier.dto.DtoToken;
+
 public interface JsonService {
+
+	DtoToken getDtoToken(String result);
 
 }
