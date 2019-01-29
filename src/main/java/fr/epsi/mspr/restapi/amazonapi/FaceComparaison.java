757bdefad267faea97b8f2a22f593cfa6412533f
shyndard <shyndard@gmail.com>
2019-01-29 08:29:42 +0100

    Ajout variables environnement + reconnaissance faciale

---
+++ src/main/java/fr/epsi/mspr/restapi/amazonapi/FaceComparaison.java
@@ -0,0 +1,58 @@
+package fr.epsi.mspr.restapi.amazonapi;
+
+import java.net.URI;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class FaceComparaison {
+	
+	private static final String subscriptionKey = "d87325fc69ec41bf85fdd87c7b2de574";
+	private static final String uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/verify";
+	
+	public static double compare(String id1, String id2) {
+		
+		HttpClient httpclient = HttpClientBuilder.create().build();
+		try {
+			URIBuilder builder = new URIBuilder(uriBase);
+			URI uri = builder.build();
+			HttpPost request = new HttpPost(uri);
+
+			// Request headers.
+			request.setHeader("Content-Type", "application/json");
+			request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+			// Request body.
+			StringEntity reqEntity = new StringEntity("{\"faceId1\": \"" + id1 + "\", \"faceId2\": \"" + id2 + "\"}");
+			request.setEntity(reqEntity);
+
+			// Execute the REST API call and get the response entity.
+			HttpResponse response = httpclient.execute(request);
+			HttpEntity entity = response.getEntity();
+			if (entity == null) {
+				return 0;
+			} else {
+				String jsonString = EntityUtils.toString(entity).trim();
+				if (jsonString.charAt(0) == '[') {
+					JSONArray jsonArray = new JSONArray(jsonString);
+					return jsonArray.getDouble(0);
+				} else if (jsonString.charAt(0) == '{') {
+					JSONObject jsonObject = new JSONObject(jsonString);
+					return jsonObject.getDouble("confidence");
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+}
---
+++ src/test/java/fr/epsi/mspr/restapi/FaceRecognitionTest.java
@@ -0,0 +1,29 @@
+package fr.epsi.mspr.restapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import fr.epsi.mspr.restapi.amazonapi.FaceComparaison;
+import fr.epsi.mspr.restapi.amazonapi.FaceRecoginition;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FaceRecognitionTest {
+
+	@Test
+	public void apiRecognitionTest() {
+		String id1 = FaceRecoginition.getFaceId("http://img.shyndard.eu/BECHMHd.jpg");
+		System.out.println(id1);
+		String id2 = FaceRecoginition.getFaceId("http://img.shyndard.eu/MRV1Tdv.jpg");
+		System.out.println(id2);
+		double comparaison = FaceComparaison.compare(id1, id2);
+		System.out.println("Pourcentage de ressemblance : " + comparaison);
+	}
+	
+	@Test
+	public void apiComparaisonTest() {
+		FaceComparaison.compare(null, null);
+	}
+}
