1444adc2482f2dccd6c2b41604bce5cd1bcc180f
shyndard <shyndard@gmail.com>
2019-02-20 10:38:54 +0100

    Suppression cascade

--- src/main/java/fr/epsi/mspr/restapi/dao/entity/Guardian.java
+++ src/main/java/fr/epsi/mspr/restapi/dao/entity/Guardian.java
@@ -36,7 +36,7 @@ public class Guardian implements Serializable {
 	private boolean administrator;
 	@Column(name="guardian_fullname")
 	private String fullname;
-	@OneToMany( mappedBy="guardian", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@OneToMany( mappedBy="guardian", fetch = FetchType.EAGER)
 	private Set<Borrow> borrow;
 	
 	public long getId() {
